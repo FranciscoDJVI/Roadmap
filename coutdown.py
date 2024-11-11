a4cf798329ca9aa6ca03baec262611896ff4bf46
Francisco Vanegas <vanegasfrancisco415@gmail.com>
2024-11-11 15:10:09 -0500

    agregando hilos

--- coutdown.py
+++ coutdown.py
@@ -6,38 +6,27 @@ import os
 # Fecha inicial ingresado por el usuario.
 final_date = input("\nIntroducce la fecha inicial dd/mm/aa hh:mm:ss ")
 
-
-    # Función para borrar la termina en cada conteo.
-def borrar_terminal():
-
-    if os.name == 'nt':
-        os.system('cls')
-
-    # Función para realizar la cuenta regresiva tomando la fecha ingresada por el usuario hasta la fecha acutal.
+# Función para realizar la cuenta regresiva tomando la fecha ingresada por el usuario hasta la fecha acutal.
 def countdown():
-
     final_date_format = datetime.datetime.strptime(final_date, "%d/%m/%Y %H:%M")
-
     fecha_now= final_date_format.now()
-        
     total = final_date_format - fecha_now 
         
     print("Cuenta regresiva para el lanzamiento de Mouredev Pro...")
-    print(f"Faltan {total.days} = dias") 
-    print(f"{total.seconds // 3600} = horas")
-    print(f"{total.seconds // 60 % 60} = minutos") 
-    print(f"{total.seconds % 60} = segundos") 
-    print(f"para terminar...")
+    print(f"Faltan {total.days} dias, {total.seconds // 3600} horas, {total.seconds // 60 % 60} minutos, {total.seconds % 60} segundos") 
         
     sleep(1)
         
-    borrar_terminal()
+    os.system("cls")
 
     if total.days == 00 and total.seconds == 00:
-        print("Felicidades ha llegado el dia")
+        print("Ha llegado el dia")
     else:
         countdown()
 
-    #'07/11/2024 20:45:00'
 
-countdown()
+#07/11/2024 20:45:00
+
+thread = threading.Thread(target=countdown)
+thread = threading.Thread.start(thread)
+thread = threading.Thread.join()
